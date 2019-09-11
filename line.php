ba1527d29ba37d5a2f984e9794d9ed84f126cc49
poi1924 <45979451+poi1924@users.noreply.github.com>
2019-09-12 00:55:33 +0700

    Update line.php

--- line.php
+++ line.php
@@ -2,7 +2,8 @@
   
 
 function send_LINE($msg){
- $access_token = 'qLip9omRdSnsaKFlsWmCCx9pdvAcRd1CGb6XfH/K3aKVgmHS4Eh/a35I8S1q8XVCZQVJUVIPa2B/c1ZJHfEyA8vUgqlUeIfqTkw607IKQ7yCasUHW34wj+CGzB6bOafYNDSGkh87GIr+Tns7fqFqVAdB04t89/1O/w1cDnyilFU='; 
+ $access_token = 'FE6SNDANIq5RD+bf/Bj2E9CUbUBpCedmSiPuHd4sOhwURx9lXhC2kBpoLpVdPi0Vi+BcyVpnYo5o+5jnJnRHD9U4KgM4T5Xt2JIeua7N+aLTQ8L/ZRoE7/EkeF6SwLp3tcMf5XbCOnytW3jff+ZKzwdB04t89/1O/w1cDnyilFU=
+'; 
 
   $messages = [
         'type' => 'text',
@@ -14,7 +15,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'to' => 'U4a1d30cba4ce0aff87d95c691e8af3a4',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
