a1791a94b8c16887701811f1fc4612b11f70f28e
poi1924 <45979451+poi1924@users.noreply.github.com>
2019-09-12 03:09:27 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -9,7 +9,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['ESP'])) {
+if (is_null($events['ESP'])) {
 	
 	send_LINE($events['ESP']);
 		
