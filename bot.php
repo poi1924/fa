62ca426df2df9b066af189275044f5849c2e687d
poi1924 <45979451+poi1924@users.noreply.github.com>
2019-09-12 01:05:18 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,9 +21,22 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 
 	}
+			if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+
+			$Topic = "NodeMCU1" ;
+			getMqttfromlineMsg($Topic,$text);
+			   
+			
+		}
 }
 $Topic = "NodeMCU1" ;
-$text = "Test";
+$text = "1";
 getMqttfromlineMsg($Topic,$text);
 echo "OK3";
 ?>
